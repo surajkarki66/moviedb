fc95f5f360783d201be3804610d8702e12ae2904
surajkarki66 <suraj.karki500@protonmail.com>
2021-10-02 14:48:14 +0545

    Added some extra details of movie

--- client/src/components/MovieDetail/MovieInfo.js
+++ client/src/components/MovieDetail/MovieInfo.js
@@ -1,3 +1,4 @@
+import moment from "moment";
 import React from "react";
 import { Descriptions } from "antd";
 
@@ -5,24 +6,43 @@ const MovieInfo = (props) => {
   const { movie } = props;
 
   return (
-    <Descriptions title="MOVIE INFORMATION" bordered style={{textAlign: "center", marginTop: "30px"}}>
+    <Descriptions
+      title="MOVIE INFORMATION"
+      bordered
+      style={{ textAlign: "center", marginTop: "30px" }}
+    >
       <Descriptions.Item label="TITLE">
-        {movie.original_title}
+        <b> {movie.original_title}</b>
       </Descriptions.Item>
       <Descriptions.Item label="RELEASE DATE">
-        {movie.release_date}
+        <b> {movie.release_date && moment(movie.release_date).format("LL")}</b>
       </Descriptions.Item>
-      <Descriptions.Item label="REVENUE">{movie.revenue}</Descriptions.Item>
-      <Descriptions.Item label="RUNTIME (MINUTE)">{movie.runtime}</Descriptions.Item>
+      <Descriptions.Item label="PRODUCTION COMPANY">
+        <b>{movie.production_companies[0].name}</b>
+      </Descriptions.Item>
+
+      <Descriptions.Item label="RUNTIME (MINUTE)">
+        <b>{movie.runtime}</b>
+      </Descriptions.Item>
       <Descriptions.Item label="VOTE AVERAGE">
-        {movie.vote_average}
+        <b> {movie.vote_average}</b>
       </Descriptions.Item>
       <Descriptions.Item label="VOTE COUNT">
-        {movie.vote_count}
+        <b> {movie.vote_count}</b>
       </Descriptions.Item>
-      <Descriptions.Item label="STATUS">{movie.status}</Descriptions.Item>
+      <Descriptions.Item label="STATUS">
+        <b>{movie.status}</b>
+      </Descriptions.Item>
       <Descriptions.Item label="POPULARITY">
-        {movie.popularity}
+        <b> {movie.popularity}</b>
+      </Descriptions.Item>
+      <Descriptions.Item label="REVENUE">
+        <b>{movie.revenue}</b>
+      </Descriptions.Item>
+      <Descriptions.Item label="LANGUAGES">
+        {movie.spoken_languages.map((l) => (
+          <b key={l.english_name}>{l.english_name} | </b>
+        ))}
       </Descriptions.Item>
     </Descriptions>
   );
