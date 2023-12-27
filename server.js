cf64a6e16d81e89d9a2852b5e324d0082df9538f
surajkarki <suraj.karki500@protonmail.com>
2023-12-27 23:10:14 +0100

    Updated status api

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use("/api/like", likeRouter);
 app.use("/", (_, res) => {
   return res
     .status(200)
-    .json({ success: true, message: "API is successfully working!" });
+    .json({ success: true, message: "Welcome to the MovieDB API!!!" });
 })
 
 const port = process.env.PORT || 5000;
